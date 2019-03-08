a30597ca12069b99732e711962cabe08ace17f72
Azuki <ogram.std@gmail.com>
2019-03-08 14:46:37 +0900

    feat: add another source to ja2sci
    added taxonomy_jp_dic and improved dictionary creating process

--- spsearch/ja2sci/dictionary/generate.py
+++ spsearch/ja2sci/dictionary/generate.py
@@ -3,7 +3,20 @@ import pickle
 from pathlib import Path
 
 here = Path(__file__).parent
-df = pd.read_csv(here / 'species_names.latin_vs_japanese.utf8.txt', sep='\t', header=None)
-dictionary = df.iloc[:, [1,0]].set_index(1).to_dict()[0]
+dictionary = {}
+
+
+# taxonomy_jp_dic
+filepath = here/'raw'/'taxonomy_jp_dic.2008-03-07.txt'
+df = pd.read_csv(str(filepath), sep='\t', header=None)
+dictionary.update(df.iloc[:, [1, 0]].set_index(1).to_dict()[0])
+
+
+# species_names.latin_vs_japanese
+filepath = here/'raw'/'species_names.latin_vs_japanese.utf8.txt'
+df = pd.read_csv(str(filepath), sep='\t', header=None)
+dictionary.update(df.iloc[:, [1, 0]].set_index(1).to_dict()[0])
+
+
 with (here / 'ja2sci.pkl').open('wb') as f:
     pickle.dump(dictionary, f)
