783eeea79d6834488708d9b7ef9a7b3419c90dc9
Azuki <ogram.std@gmail.com>
2019-03-25 07:27:43 +0900

    fix: Lacking import on redlist.__init__

--- spsearch/redlist/__init__.py
+++ spsearch/redlist/__init__.py
@@ -1,3 +1,5 @@
 from .handler import RedListApiHandler
 from .species import Species
 from .habitats import Habitat
+from .threats import Threat
+from .conservation_measures import ConservationMeasure
